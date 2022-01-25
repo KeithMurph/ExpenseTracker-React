1ccbb283900eb30ace683257f2ea4e72fc9fff24
KeithMurph <keithmurphy94@gmail.com>
2022-01-25 02:09:01 -0800

    Dynamic button to hide new expense form

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
+// import ExpenseItem from "./ExpenseItem";
 import Card from "./Card";
 import "./Expenses.css";
 import ExpensesFilter from "./NewExpense/ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2022");
@@ -13,7 +14,9 @@ const Expenses = (props) => {
 
 const filteredExpenses = props.items.filter( expense => {
   return expense.date.getFullYear().toString() === filteredYear;
-})
+});
+
+
   return (
     <div>
       <Card className="expenses">
@@ -21,15 +24,7 @@ const filteredExpenses = props.items.filter( expense => {
           selected={filteredYear}
           onChangeFilter={filterChangedHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
-    
+      <ExpensesList items ={filteredExpenses}/>
       </Card>
     </div>
   );
